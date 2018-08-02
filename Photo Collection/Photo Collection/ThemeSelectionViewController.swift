c0677d5a2116bdf207aa0d0bfb4d5494808edd06
Samantha Gatt <smfgatt@gmail.com>
2018-08-02 16:25:52 -0400

    Configures theme selection view controller scene

---
+++ Photo Collection/Photo Collection/ThemeSelectionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ThemeSelectionViewController.swift
+//  Photo Collection
+//
+//  Created by Samantha Gatt on 8/2/18.
+//  Copyright © 2018 Samantha Gatt. All rights reserved.
+//
+
+import UIKit
+
+class ThemeSelectionViewController: UIViewController {
+
+    
+    // MARK: - Actions
+    
+    @IBAction func selectGreenTheme(_ sender: Any) {
+        
+    }
+    
+    @IBAction func selectDarkTheme(_ sender: Any) {
+        
+    }
+    
+    
+    
+    // MARK: - Properties
+    
+    var themeHelper: ThemeHelper?
+
+}
