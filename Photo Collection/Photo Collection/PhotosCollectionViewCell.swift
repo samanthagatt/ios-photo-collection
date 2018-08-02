4e86dce7ebd02fc5545fe75fff63cb2b1101581a
Samantha Gatt <smfgatt@gmail.com>
2018-08-02 15:51:23 -0400

    Configures collection view controller scene

---
+++ Photo Collection/Photo Collection/PhotosCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  PhotosCollectionViewCell.swift
+//  Photo Collection
+//
+//  Created by Samantha Gatt on 8/2/18.
+//  Copyright © 2018 Samantha Gatt. All rights reserved.
+//
+
+import UIKit
+
+class PhotosCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var photoLabel: UILabel!
+}
